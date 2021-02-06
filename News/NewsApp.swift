1993f797ce6baacd0c82712f14c738a553d09979
developer <developer@robosoftin.com>
2021-02-06 09:20:01 +0530

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Nagaraj on 06/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
